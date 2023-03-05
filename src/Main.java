e1e54477e308420c5904254764c8fbb8d40c969e
scaledm <dimanator2016@gmail.com>
2023-03-05 23:28:52 +0300

    2 while cycles. TODO: understatd how to make limit of random

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a,i=0;
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        System.out.println("write 1 if you want to enter numbers of masiv by yoursalfe and 2 - random");
+        int ch= sc.nextInt(), n=5;
+       int A[];
+       A= new int [n];
+       while (ch==1){
+A[i]= sc.nextInt();
+i++;
+    if(i==n){break;}
+       }
+
+        while (ch==2){
+            A[i]= rd.nextInt()*100;
+            i++;
+            if(i==n){break;}
+        }
+        for(i=0;i<n;i++)
+        {
+            System.out.print(" |"+A[i]);
+        }
     }
 }
